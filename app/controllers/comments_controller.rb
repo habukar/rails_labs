3aeb2ef507ed3d70ac626c57daa6730e7af0f54c
EC2 Default User <ec2-user@ip-172-31-22-199.us-east-2.compute.internal>
2019-11-05 16:11:10 +0000

    Add destroy functionality for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,17 @@ class CommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @commment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
-end
+    end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article)
+    end
 
 private 
-def comment_params
-    params.require(:comment).permit(:commenter, :body)
-end
+    def comment_params
+        params.require(:comment).permit(:commenter, :body)
+    end
 end
